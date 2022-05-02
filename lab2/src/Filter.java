9903ba6ced5a00f80e325111c24a6a6fc1757699
QiWu-57 <2193224374@qq.com>
2022-05-02 22:57:09 +0800

    finish lab2-exercise1 ,notice Joni-fetchnext()

---
+++ lab2/src/Filter.java
@@ -0,0 +1,97 @@
+package simpledb.execution;
+
+import simpledb.transaction.TransactionAbortedException;
+import simpledb.common.DbException;
+import simpledb.storage.Tuple;
+import simpledb.storage.TupleDesc;
+
+import java.util.*;
+
+/**
+ * Filter is an operator that implements a relational select.
+ */
+public class Filter extends Operator {
+
+    private static final long serialVersionUID = 1L;
+    private Predicate p;
+    private OpIterator child;
+    /**
+     * Constructor accepts a predicate to apply and a child operator to read
+     * tuples to filter from.
+     *
+     * @param p
+     *            The predicate to filter tuples with
+     * @param child
+     *            The child operator
+     */
+    public Filter(Predicate p, OpIterator child) {
+        // some code goes here
+        this.p = p;
+        this.child = child;
+    }
+
+    public Predicate getPredicate() {
+        // some code goes here
+        return p;
+    }
+
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        return child.getTupleDesc();
+    }
+
+    public void open() throws DbException, NoSuchElementException,
+            TransactionAbortedException {
+        // some code goes here
+        child.open();
+        super.open();
+    }
+
+    public void close() {
+        // some code goes here
+        super.close();
+        child.close();
+    }
+
+    public void rewind() throws DbException, TransactionAbortedException {
+        // some code goes here
+        child.rewind();
+    }
+
+    /**
+     * AbstractDbIterator.readNext implementation. Iterates over tuples from the
+     * child operator, applying the predicate to them and returning those that
+     * pass the predicate (i.e. for which the Predicate.filter() returns true.)
+     *
+     * @return The next tuple that passes the filter, or null if there are no
+     *         more tuples
+     * @see Predicate#filter
+     */
+    protected Tuple fetchNext() throws NoSuchElementException,
+            TransactionAbortedException, DbException {
+        // some code goes here
+        while ( child.hasNext() ){
+            Tuple tuple = child.next();
+            if ( p.filter(tuple) ) {
+                return tuple;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public OpIterator[] getChildren() {
+        // some code goes here
+        return new OpIterator[] {
+            child
+        };
+    }
+
+    @Override
+    public void setChildren(OpIterator[] children) {
+        // some code goes here
+        children[0] = child;
+    }
+
+}
